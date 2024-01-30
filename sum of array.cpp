fee36d0f2b2f0be0f7632199498e4d5545097c37
svlalli <157869493+svlalli@users.noreply.github.com>
2024-01-30 22:31:43 +0530

    Add files via upload

---
+++ sum of array.cpp
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+
+int main(){
+     int arr[6], size;
+    int *ptr, sum = 0;
+
+    printf("Enter size  of the array: ");
+    scanf("%d", &size);
+
+    printf("Enter array elements: ");
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+    ptr = arr;
+
+    for (int i = 0; i < size; i++) {
+        sum = sum + *ptr;
+        ptr++; 
+    }
+
+    printf("The sum of array elements is: %d", sum);
+
+    return 0;
+
+}
